68cde5f0acd9ab883507f107437ca59061940cdb
Sandra Rodgers <sandra.rodgers@deepgram.com>
2023-11-06 16:27:16 -0600

    update example

--- test/example.py
+++ test/example.py
@@ -3,12 +3,10 @@ from deepgram_captions.webvtt import webvtt
 from deepgram_captions.converters import AssemblyAIConverter, DeepgramConverter
 import json
 
-# Specify the path to your JSON file
 json_file_dg_transcription = "test/dg_transcription.json"
 json_file_dg_utterances = "test/dg_utterances.json"
 json_file_dg_speakers = "test/dg_speakers.json"
 json_file_dg_speakers_no_utterances = "test/dg_speakers_no_utterances.json"
-
 json_file_assemblyai_transcription = "test/assemblyai_transcription.json"
 json_file_assemblyai_utterances = "test/assemblyai_utterances.json"
 
@@ -18,37 +16,27 @@ dg_speakers = None
 dg_speakers_no_utterances = None
 assemblyai_transcription = None
 assemblyai_utterances = None
-# Open and load the JSON data
+
 with open(json_file_dg_transcription, "r") as json_file:
     dg_transcription = json.load(json_file)
-
 with open(json_file_dg_utterances, "r") as json_file:
     dg_utterances = json.load(json_file)
-
 with open(json_file_dg_speakers, "r") as json_file:
     dg_speakers = json.load(json_file)
-
 with open(json_file_dg_speakers_no_utterances, "r") as json_file:
     dg_speakers_no_utterances = json.load(json_file)
-
 with open(json_file_assemblyai_transcription, "r") as json_file:
     assemblyai_transcription = json.load(json_file)
-
 with open(json_file_assemblyai_utterances, "r") as json_file:
     assemblyai_utterances = json.load(json_file)
 
+# Uncomment a section to test the converter:
 
-assembly = AssemblyAIConverter(assemblyai_utterances)
-captions = webvtt(assembly)
+deepgram = DeepgramConverter(dg_transcription)
+captions = srt(deepgram)
 print(captions)
 
-# deepgram = DeepgramConverter(dg_transcription)
-# captions = srt(deepgram)
+# assembly = AssemblyAIConverter(assemblyai_utterances)
+# captions = webvtt(assembly)
 # print(captions)
 
-# srt_result = srt(dg_utterances)
-# print("SRT Result:\n", srt_result)
-
-
-# result = seconds_to_timestamp(500, "%H:%M:%S,%f")
-# print(result)

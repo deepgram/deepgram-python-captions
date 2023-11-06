d4bb32a891fd57febea7ad8d66aa52545e79782b
Sandra Rodgers <sandra.rodgers@deepgram.com>
2023-11-06 12:45:17 -0600

    add logic for speakers

--- deepgram_captions/converters.py
+++ deepgram_captions/converters.py
@@ -2,24 +2,40 @@ from .helpers import chunk_array
 
 class DeepgramConverter:
   def __init__(self, dg_response):
-    # get the deepgram response.
     self.response = dg_response
 
   def get_lines(self, line_length: int = 8):
     results = self.response['results']
     content = []
-    # check if response has utterances. If so, loop over each utterance and check if the utterances length is greater than the line_length. If so break down the given utterance.words array into smaller chunks, then push into content array
-    if results['utterances']:
+
+    if results.get('utterances'):
       for utterance in results['utterances']:
         if len(utterance['words']) > line_length:
           content.extend(chunk_array(utterance['words'], line_length))
         else:
           content.append(utterance['words'])
-    
-    # if no utterances, break down results.channels[0].alternatives[0].words into smaller chunks, then push into content array
 
     else:
-      content.extend(chunk_array(results['channels'][0]['alternatives'][0]['words'], line_length))
+        words = results['channels'][0]['alternatives'][0]['words']
+        diarize = 'speaker' in words[0] if words else False  # Check if diarization was used
+        buffer = []
+        current_speaker = 0
+
+        for word in words:
+            if diarize and word.get('speaker', 0) != current_speaker:
+                content.append(buffer)
+                buffer = []
+
+            if len(buffer) == line_length:
+                content.append(buffer)
+                buffer = []
+
+            if diarize:
+                current_speaker = word.get('speaker', 0)
+            
+            buffer.append(word)
+        
+        content.append(buffer)
   
     return content
   
--- main.py
+++ main.py
@@ -6,11 +6,16 @@ import json
 # Specify the path to your JSON file
 json_file_dg_transcription = "test/dg_transcription.json"
 json_file_dg_utterances = "test/dg_utterances.json"
+json_file_dg_speakers = "test/dg_speakers.json"
+json_file_dg_speakers_no_utterances = "test/dg_speakers_no_utterances.json"
+
 json_file_assemblyai_transcription = "test/assemblyai_transcription.json"
 json_file_assemblyai_utterances = "test/assemblyai_utterances.json"
 
 dg_transcription = None
 dg_utterances = None
+dg_speakers = None
+dg_speakers_no_utterances = None
 assemblyai_transcription = None
 assemblyai_utterances = None
 # Open and load the JSON data
@@ -20,6 +25,12 @@ with open(json_file_dg_transcription, "r") as json_file:
 with open(json_file_dg_utterances, "r") as json_file:
     dg_utterances = json.load(json_file)
 
+with open(json_file_dg_speakers, "r") as json_file:
+    dg_speakers = json.load(json_file)
+
+with open(json_file_dg_speakers_no_utterances, "r") as json_file:
+    dg_speakers_no_utterances = json.load(json_file)
+
 with open(json_file_assemblyai_transcription, "r") as json_file:
     assemblyai_transcription = json.load(json_file)
 
@@ -27,13 +38,13 @@ with open(json_file_assemblyai_utterances, "r") as json_file:
     assemblyai_utterances = json.load(json_file)
 
 
-assembly = AssemblyAIConverter(assemblyai_utterances)
-captions = srt(assembly)
-print(captions)
-
-# deepgram = DeepgramConverter(dg_utterances)
-# captions = srt(deepgram)
+# assembly = AssemblyAIConverter(assemblyai_utterances)
+# captions = srt(assembly)
 # print(captions)
+
+deepgram = DeepgramConverter(dg_utterances)
+captions = srt(deepgram)
+print(captions)
 
 # srt_result = srt(dg_utterances)
 # print("SRT Result:\n", srt_result)
